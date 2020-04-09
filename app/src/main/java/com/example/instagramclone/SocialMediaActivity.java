5439ed58f08a49940e5ba5cf18db02606adefaa8
VibhaSN <vibha.navale11@gmail.com>
2020-04-09 23:58:18 +0530

    Tab Layout Setup

--- app/src/main/java/com/example/instagramclone/SocialMediaActivity.java
+++ app/src/main/java/com/example/instagramclone/SocialMediaActivity.java
@@ -1,14 +1,40 @@
 package com.example.instagramclone;
 
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
-public class SocialMediaActivity extends AppCompatActivity {
+import com.google.android.material.tabs.TabLayout;
+
+public class SocialMediaActivity extends AppCompatActivity implements SharePictureTab.OnFragmentInteractionListener {
+
+    private androidx.appcompat.widget.Toolbar toolbar;
+    private ViewPager viewPager;
+    private TabLayout tabLayout;
+    private TabAdapter tabAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_social_media);
+
+        setTitle("Social Media App");
+
+        toolbar = findViewById(R.id.myToolbar);
+        setSupportActionBar(toolbar);
+
+        viewPager = findViewById(R.id.viewPager);
+        tabAdapter = new TabAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(tabAdapter);
+
+        tabLayout = findViewById(R.id.tabLayout);
+        tabLayout.setupWithViewPager(viewPager, false);
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
